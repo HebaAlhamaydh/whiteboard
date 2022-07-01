64a261d89f560042f0fa55cae224bc91f00b6033
HebaAlhamaydh <Eng_heba_hamaydh@hotmail.com>
2022-07-01 06:23:25 +0300

    do

--- public/script.js
+++ public/script.js
@@ -1,13 +1,20 @@
+"use strict";
 let canvas=document.getElementById('canvas')
+var io=io.connect('http://localhost:5000')
 canvas.width = 0.98 * window.innerWidth;
 canvas.height = window.innerHeight;
-var io=io.connect('http://localhost:8080/')
 
 let ctx = canvas.getContext("2d");
-
 let x;
 let y;
 let mouseDown=false;
+io.on("onDraw",(data)=>{
+    ctx.lineTo(data.x, data.y);
+    ctx.stroke();
+})
+io.on("ondown",(data)=>{
+    ctx.moveTo(data.x,data.y);
+})
 window.onmousedown=(e)=>{
     ctx.moveTo(x,y);
     io.emit("down",{x,y})
@@ -16,13 +23,7 @@ window.onmousedown=(e)=>{
 window.onmouseup=(e)=>{
     mouseDown=false;
 }
-io.on("ondraw",({x,y})=>{
-    ctx.lineTo(x,y);
-    ctx.stroke();
-})
-io.on('ondown',({x,y})=>{
-    ctx.moveTo(x,y);
-})
+
 window.onmousemove=(e)=>{
 x=e.clientX;
 y=e.clientY;
